fbeef2c23d4a61e4a9bb43ac6c08229ee2bb37e0
Yosef Steinberg <steinbergyosef@gmail.com>
2025-05-12 13:04:43 +0300

    Hello

---
+++ cs_files 2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cs_files_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
